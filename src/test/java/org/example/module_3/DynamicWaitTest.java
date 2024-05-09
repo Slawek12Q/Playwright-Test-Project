8b77bfa1a18bc837cb3b277473163d1103cea10f
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-05-10 00:18:36 +0200

    Test add static import and code refactor

--- src/test/java/org/example/module_3/DynamicWaitTest.java
+++ src/test/java/org/example/module_3/DynamicWaitTest.java
@@ -2,13 +2,12 @@ package org.example.module_3;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.WaitUntilState;
 import org.example.common.BaseTest;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Paths;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class DynamicWaitTest extends BaseTest {
 
@@ -17,12 +16,12 @@ public class DynamicWaitTest extends BaseTest {
         page.navigate("https://the-internet.herokuapp.com/dynamic_loading/1", new Page.NavigateOptions().setWaitUntil(WaitUntilState.NETWORKIDLE));
         Locator text = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Hello World"));
 
-        PlaywrightAssertions.assertThat(text).not().isVisible();
+        assertThat(text).not().isVisible();
 
         Locator startButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Start"));
 
         startButton.click();
-        PlaywrightAssertions.assertThat(text).isVisible();
+        assertThat(text).isVisible();
 
 //        page.click("div#start > button");
 //        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot/dynamic_wait.png")));
@@ -36,18 +35,18 @@ public class DynamicWaitTest extends BaseTest {
 
         Locator text = page.getByText("It's gone!");
 
-        PlaywrightAssertions.assertThat(text).not().isVisible();
+        assertThat(text).not().isVisible();
         removeButton.click();
-        PlaywrightAssertions.assertThat(text).isVisible();
+        assertThat(text).isVisible();
 
         Locator input = page.locator("//form/input");
 
-        PlaywrightAssertions.assertThat(input).isDisabled();
+        assertThat(input).isDisabled();
 
         Locator enableButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Enable"));
         enableButton.click();
 
-        PlaywrightAssertions.assertThat(input).isEnabled();
+        assertThat(input).isEnabled();
 
     }
 }
