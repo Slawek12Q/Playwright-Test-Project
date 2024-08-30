ac90d601c73a96df4919ee6fc2c6f735dfbf31c6
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-08-31 01:44:07 +0200

    Extend test class

--- src/test/java/org/example/mofule_6_rest/ReadAllUsersTest.java
+++ src/test/java/org/example/mofule_6_rest/ReadAllUsersTest.java
@@ -2,20 +2,29 @@ package org.example.mofule_6_rest;
 
 import com.google.gson.Gson;
 import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import org.example.common.BaseApiTest;
 import org.example.mofule_6_rest.dto.UserDto;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReadAllUsersTest extends BaseApiTest {
 
     @Test
     public void readAllUsersTest() {
         APIResponse response = requestContext.get("users");
+        PlaywrightAssertions.assertThat(response).isOK();
 
-        UserDto[] userDtos = new Gson().fromJson(response.text(), UserDto[].class);
+        List<UserDto> userDtos = Arrays.asList(new Gson().fromJson(response.text(), UserDto[].class));
 
         for(UserDto userDto: userDtos){
             log.info(userDto.toString());
         }
+
+        List<String> emails = userDtos.stream().map(k -> k.getEmail()).toList();
+        log.info(emails.toString());
     }
 }
