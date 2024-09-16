b799d6106519c3f2eeb706776a12855fb77c358b
Slawomir Guzik <slawek.guzik1@gmail.com>
2024-09-17 00:30:33 +0200

    Add assertions to the test

--- src/test/java/org/example/mofule_6_rest/PartialUpdateUserTest.java
+++ src/test/java/org/example/mofule_6_rest/PartialUpdateUserTest.java
@@ -1,10 +1,13 @@
 package org.example.mofule_6_rest;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.options.RequestOptions;
 import org.example.common.BaseApiTest;
+import org.example.mofule_6_rest.dto.UserDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PartialUpdateUserTest extends BaseApiTest {
@@ -17,5 +20,11 @@ public class PartialUpdateUserTest extends BaseApiTest {
         APIResponse apiResponse = requestContext.patch("users/10", RequestOptions.create().setData(userUpdate));
         PlaywrightAssertions.assertThat(apiResponse).isOK();
         log.info(apiResponse.text());
+
+        UserDto userDto = new Gson().fromJson(apiResponse.text(), UserDto.class);
+
+        log.info(userDto.toString());
+
+        Assertions.assertTrue(userDto.getEmail().equals("test.email@test.pl"));
     }
 }
